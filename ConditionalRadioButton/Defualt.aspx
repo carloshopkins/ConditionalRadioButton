12796ac28e06d325a6f1d1c73cbbda42a58d4d5b
Kuhgelfische <Kuhgelfische@DESKTOP-7FBLCO2>
2017-03-25 09:00:37 -0400

    Add project files.

---
+++ ConditionalRadioButton/Defualt.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Defualt.aspx.cs" Inherits="ConditionalRadioButton.Defualt" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <p>
+            What is your note taking preference?\</p>
+        <p>
+            <asp:RadioButton ID="penRadio" runat="server" GroupName="notes" Text="Pen" />
+        </p>
+        <p>
+            <asp:RadioButton ID="pencilRadio" runat="server" GroupName="notes" Text="Pencil" />
+        </p>
+        <p>
+            <asp:RadioButton ID="laptopRadio" runat="server" GroupName="notes" Text="Laptop" />
+        </p>
+        <p>
+            <asp:RadioButton ID="tabletRadio" runat="server" GroupName="notes" Text="Tablet" />
+        </p>
+        <asp:Button ID="submitButton" runat="server" Text="Submit" OnClick="submitButton_Click" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Image ID="resultImage" runat="server" />
+    </form>
+</body>
+</html>
---
+++ ConditionalRadioButton/Defualt.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing.Imaging;
+
+namespace ConditionalRadioButton
+{
+    public partial class Defualt : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            resultImage.Visible = false;
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            if (pencilRadio.Checked)
+            {
+                
+                resultLabel.Text = "So you're an avid pencil user?";
+                resultImage.ImageUrl = "~/pencil.jpg";
+                resultImage.Visible = true;
+
+            }
+            else if(penRadio.Checked){
+
+                resultLabel.Text = "I too enjoy using pens";
+                resultImage.ImageUrl = "~/pen.jpg";
+                resultImage.Visible = true;
+            }
+            else if(laptopRadio.Checked){
+
+                resultLabel.Text = "Thats nice, but you know you're just browsing reddit";
+                resultImage.ImageUrl = "~/laptop.jpg";
+                resultImage.Visible = true;
+
+            }
+            else if (tabletRadio.Checked)
+            {
+                resultLabel.Text = "Lets be real tablets suck for taking notes";
+                resultImage.ImageUrl = "~/tablet.jpg";
+                resultImage.Visible = true;
+            }
+            else
+            {
+                resultLabel.Text = "You need to select something";
+                resultImage.Visible = false;
+            }
+        }
+    }
+}
